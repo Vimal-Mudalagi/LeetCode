2cc905d6207abf89bfc9ca2731a1d3b975bb1917
Vimal Mudalagi <134709589+Vimal-Mudalagi@users.noreply.github.com>
2024-11-02 19:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (52.44%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isCircularSentence(self, sentence):
+        """
+        :type sentence: str
+        :rtype: bool
+        """
+        words=sentence.split()
+        for i in range(len(words)-1):
+            if words[i][-1]!=words[i+1][0]:return False
+        return words[-1][-1]==words[0][0]
+
+                     

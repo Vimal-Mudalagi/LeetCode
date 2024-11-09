1a283e019cdae13c29af8d40d344f9f425ab8422
Vimal Mudalagi <134709589+Vimal-Mudalagi@users.noreply.github.com>
2024-11-09 20:23:55 +0530

    Update maximum-xor-for-each-query.py

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -1,10 +1,12 @@
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
         n=len(nums)
-        prev=[]
+        prev=[]      
         mask=(1<<maximumBit)-1
+        
         for x in nums:
             prev.append(mask^x)
             mask=mask^x
         prev.reverse()
+        
         return prev
